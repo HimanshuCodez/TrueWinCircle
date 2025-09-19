64377930257a419ee2cf7095d01a2dd222208a4b
Himanshu gaur <himanshugaur055@gmail.com>
2025-09-19 15:26:23 +0530

    refer

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -63,9 +63,9 @@ export default function Navbar() {
                 <div className="absolute right-0 mt-2 w-40 bg-white text-black rounded-md shadow-lg z-10">
                   <Link to="/Wallet" className="block px-4 py-2 hover:bg-gray-100">Wallet</Link>
                   <Link to="/Withdraw" className="block px-4 py-2 hover:bg-gray-100">Withdraw</Link>
+                  <Link to="/Reffer" className="block px-4 py-2 hover:bg-gray-100">Reffer & Earn</Link>
                   <Link to="/AddCash" className="block px-4 py-2 hover:bg-gray-100">Add Cash</Link>
                   <Link to="/History" className="block px-4 py-2 hover:bg-gray-100">History</Link>
-                  <Link to="/Reffer" className="block px-4 py-2 hover:bg-gray-100">Reffer & Earn</Link>
                   <button onClick={handleLogout} className="block w-full text-left px-4 py-2 hover:bg-gray-100">Logout</button>
                 </div>
               )}
