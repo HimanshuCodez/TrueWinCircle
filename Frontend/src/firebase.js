1279ec1772fda4e77b2ba2e14e49dd7e6bc9cfa9
Himanshu gaur <himanshugaur055@gmail.com>
2025-09-22 15:31:16 +0530

    spinwheel

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCVgfkbPfkef7U3t4TLvJhgArzTcNrzIZc",
   authDomain: "true-win-circle.firebaseapp.com",
   projectId: "true-win-circle",
-  storageBucket: "true-win-circle.appspot.com",
+  storageBucket: "true-win-circle.firebasestorage.app",
   messagingSenderId: "631622939115",
   appId: "1:631622939115:web:74d1900eae86a853ffe9ae",
   measurementId: "G-FXQ79HW7JP"
