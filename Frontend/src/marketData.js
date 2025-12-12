390f7c16d1f5b69e524276c6ccdb7b11f287051c
Himanshu gaur <himanshugaur055@gmail.com>
2025-12-12 11:18:41 +0530

    changes time format

--- Frontend/src/marketData.js
+++ Frontend/src/marketData.js
@@ -1,5 +1,6 @@
 export const markets = [
     { name: "DELHI BAZAAR", openTime: "08:00 PM", closeTime: "06:40 PM" },
+    { name: "GHAZIABAD", openTime: "08:00 PM", closeTime: "06:40 PM" },
     { name: "DHAN KUBER", openTime: "11:00 PM", closeTime: "05:40 PM" },
     { name: "DISAWAR", openTime: "03:00 PM", closeTime: "08:40 PM" },
     { name: "FARIDABAD", openTime: "03:00 PM", closeTime: "08:40 PM" },
